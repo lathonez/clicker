1c16fb5b45973b2740166e075785f02fe6da02c2
Stephen Hazleton <shazleto@gmail.com>
2015-12-09 16:57:16 +1300

    eslint

--- www/app/app.js
+++ www/app/app.js
@@ -1,16 +1,15 @@
-import {App, Platform} from 'ionic/ionic';
-import {Page1} from './page1/page1';
-import {Page2} from './page2/page2';
-import {Page3} from './page3/page3';
+import { App, Platform } from 'ionic/ionic';
+import { Page1 } from './page1/page1';
+import { Page2 } from './page2/page2';
+import { Page3 } from './page3/page3';
 import './app.scss';
 
 
 @App({
-  templateUrl: 'app/app.html'
+  templateUrl: 'app/app.html',
 })
 export class MyApp {
   constructor(platform: Platform) {
-
     // this tells the tabs component which Pages
     // should be each tab's root Page
     this.tab1Root = Page1;
