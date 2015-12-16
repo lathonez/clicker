56cff968b9bca49bc70f137d65240f997544e4b0
Stephen Hazleton <shazleto@gmail.com>
2015-12-17 06:52:23 +1300

    Need to pass button directive through to make it button

--- www/app/components/clickerButton/clickerButton.ts
+++ www/app/components/clickerButton/clickerButton.ts
@@ -3,14 +3,16 @@
 import { Component, View } from 'angular2/angular2';
 import { Clickers } from '../../services/clickers';
 import { Clicker } from '../../models/clicker';
+import { Button } from 'ionic-framework/ionic';
 
 @Component({
   selector: 'clicker-button',
-  inputs: ['clicker: clicker'],
+  inputs: ['clicker: clicker']
 })
 
 @View({
   templateUrl: 'app/components/clickerButton/clickerButton.html',
+  directives: [Button]
 })
 
 export class ClickerButton {
