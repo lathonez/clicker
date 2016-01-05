57be15626eb55704023de2477bb1349104186898
Stephen Hazleton <shazleto@gmail.com>
2016-01-05 19:13:53 +1300

    Renaming folder to pages

--- www/app/pages/page2/page2.ts
+++ www/app/pages/page2/page2.ts
@@ -3,7 +3,7 @@
 import { Page } from 'ionic-framework/ionic';
 
 @Page({
-  templateUrl: 'app/components/page2/page2.html'
+  templateUrl: 'app/pages/page2/page2.html'
 })
 export class Page2 {
   constructor() {
