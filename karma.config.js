9dbd2c4c1f87de4ed8b3e060c48a7855780455b2
Stephen Hazleton <shazleto@gmail.com>
2016-01-06 11:45:34 +1300

    single run

--- karma.config.js
+++ karma.config.js
@@ -101,7 +101,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: true
   });
 
   if (process.env.APPVEYOR) {
