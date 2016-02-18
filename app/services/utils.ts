0621169f92849cc47048130c8aad2d0d65f0a899
Stephen Hazleton <shazleto@gmail.com>
2016-02-18 22:11:02 +1300

    Adding promiseCatchHandler

--- app/services/utils.ts
+++ app/services/utils.ts
@@ -4,6 +4,12 @@ import { AbstractControl } from 'angular2/common';
 
 export class Utils {
 
+  public static promiseCatchHandler(err: Error): void {
+    console.error('ERROR - An error has occurred inside a promise!');
+    // throw the error out to the console - http://stackoverflow.com/a/30741722
+    setTimeout(function(): void { throw err; });
+  }
+
   // bit of a hack here to reset the validation / state on the control as well as the value
   // expecting a Control.reset() method to do this but there doesn't seem to be one
   // http://stackoverflow.com/questions/33084280/how-to-reset-control-value
