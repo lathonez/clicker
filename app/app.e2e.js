9bbc4517ddce486d0266e70f3463a5cafbd81565
Daniela <daniela.galeote@gmail.com>
2016-03-10 21:00:14 +0000

    adds and passes test for deleting new clicker from page

--- app/app.e2e.js
+++ app/app.e2e.js
@@ -5,31 +5,29 @@ describe('App', () => {
   var clickerList = element(by.className('clickerList'));
 
   beforeEach(() => {
-      browser.get('');
+    browser.get('');
   });
 
   it('should have a title', () => {
-      expect(browser.getTitle()).toEqual('Ionic');
+    expect(browser.getTitle()).toEqual('Ionic');
   });
 
   it('should have <nav>', () => {
-      expect(element(by.css('ion-navbar')).isPresent()).toEqual(true);
+    expect(element(by.css('ion-navbar')).isPresent()).toEqual(true);
   });
 
   it('should have correct nav text for Home', () => {
-      expect(element(by.css('ion-navbar:first-child')).getText()).toEqual('Clickers');
+    expect(element(by.css('ion-navbar:first-child')).getText()).toEqual('Clickers');
   });
 
+  it('has a menu button that displays the left menu', () => {
+    element(by.css('.bar-button-menutoggle')).click();
+    expect(element(by.css('.toolbar-title')).getText()).toEqual('Menu');
+  });
 
   it('has an input box for new Clickers', () => {
     expect(element(by.css('.text-input')).isPresent()).toEqual(true);
   });
-
-
-  it('has a menu button that displays the left menu', () => {
-       element(by.css('.bar-button-menutoggle')).click();
-       expect(element(by.css('.toolbar-title')).getText()).toEqual('Menu');
-   });
 
   it('should add a new Clicker to the page', () => {
     clickerField.sendKeys('deal with protractor');
@@ -37,5 +35,11 @@ describe('App', () => {
     expect(clickerList.getText()).toContain('deal with protractor');
   });
 
+  it('should delete a Clicker from the page when delete button selected', () => {
+    clickerField.sendKeys('master angular');
+    clickerButton.click();
+    element(by.css('.button-outline-danger')).click();
+    expect(clickerList.getText()).not.toContain('master angular');
+  });
 
 });
