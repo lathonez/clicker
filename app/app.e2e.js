524999aab59e0ebc442c0d457bcc75033b2da80b
Daniela <daniela.galeote@gmail.com>
2016-03-10 18:05:49 +0000

    test for left menu passing

--- app/app.e2e.js
+++ app/app.e2e.js
@@ -25,17 +25,11 @@ describe('App', () => {
     expect(element(by.css('.text-input')).isPresent()).toEqual(true);
   });
 
-  xit('should have correct nav text for About', () => {
-      expect(element(by.css('sd-app sd-navbar nav a:last-child')).getText()).toEqual('ABOUT');
-
-  });
 
   it('has a menu button that displays the left menu', () => {
-      element(by.css('.bar-button-menutoggle')).click();
-      expect(element(by.css('.toolbar-title')).getText()).toEqual('Menu');
-
-  });
-
+       element(by.css('.bar-button-menutoggle')).click();
+       expect(element(by.css('.toolbar-title')).getText()).toEqual('Menu');
+   });
 
   it('should add a new Clicker to the page', () => {
     clickerField.sendKeys('deal with protractor');
