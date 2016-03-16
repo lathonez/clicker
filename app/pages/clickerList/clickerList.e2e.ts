642973d21e9fd73b41b5b3cb1e6459012331b9bc
Stephen Hazleton <shazleto@gmail.com>
2016-03-16 19:39:53 +1300

    using sendKeys workaround

--- app/pages/clickerList/clickerList.e2e.ts
+++ app/pages/clickerList/clickerList.e2e.ts
@@ -3,9 +3,9 @@ import { ElementFinder } from 'protractor';
 let clickerField: ElementFinder = element(by.css('.text-input'));
 let clickerButton: ElementFinder = element.all(by.className('button')).first();
 let removeButton: ElementFinder = element.all(by.css('.button-outline-danger')).first();
-let clickerList: ElementFinder = element.all(by.className('clickerList')).first();
+let firstClicker: ElementFinder = element.all(by.className('clickerList')).first();
 
-describe('ClickerList', () => {
+describe('firstClicker', () => {
 
   beforeEach(() => {
     browser.get('');
@@ -21,19 +21,20 @@ describe('ClickerList', () => {
     expect(element(by.css('.text-input')).isPresent()).toEqual(true);
   });
 
-  it('should add a new Clicker to the page', () => {
-    clickerField.sendKeys('deal with protractor');
+  it('should add a Clicker', () => {
+    'test clicker one'.split('').forEach((c) => clickerField.sendKeys(c))
     clickerButton.click();
-    browser.sleep(1000)
-      .then(() => {
-        expect(clickerList.getText()).toContain('deal with protractor');
-      });
+    expect(firstClicker.getText()).toEqual('test clicker one (0)');
   });
 
-  it('should delete a Clicker from the page when delete button selected', () => {
-    clickerField.sendKeys('master angular');
-    clickerButton.click();
-    removeButton.click();
-    expect(clickerList.getText()).not.toContain('master angular');
+  it('should click a Clicker', () => {
+    firstClicker.click()
+    expect(firstClicker.getText()).toEqual('test clicker one (1)')
+  });
+
+  it('should delete a Clicker', () => {
+    removeButton.click()
+    element.all(by.className('clickerList')).count()
+      .then((count) => expect(count).toEqual(0));
   });
 });
