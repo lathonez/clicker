91be221a22201ef208ac83e737968dae6d486021
Stephen Hazleton <shazleto@gmail.com>
2016-04-17 21:09:00 +1200

    jshint + prune istanbul

--- test/gulpfile.ts
+++ test/gulpfile.ts
@@ -82,21 +82,27 @@ function lint(): any {
 function debugKarma(done: Function): any {
   'use strict';
 
-  new (<any>karma).Server({
-    configFile: join(process.cwd(), TEST_DIR, 'karma.config.js'),
-    singleRun: false,
-    browsers: ['Chrome'],
-  }, done).start();
+  new (<any>karma).Server(
+    {
+      configFile: join(process.cwd(), TEST_DIR, 'karma.config.js'),
+      singleRun: false,
+      browsers: ['Chrome'],
+    },
+    done
+  ).start();
 }
 
 // run jasmine unit tests using karma with PhantomJS2 in single run mode
 function startKarma(done: Function): any {
   'use strict';
 
-  new (<any>karma).Server({
-    configFile: join(process.cwd(), TEST_DIR, 'karma.config.js'),
-    singleRun: true,
-  }, done).start();
+  new (<any>karma).Server(
+    {
+      configFile: join(process.cwd(), TEST_DIR, 'karma.config.js'),
+      singleRun: true,
+    },
+    done
+  ).start();
 }
 
 function watchTest(): any {
@@ -108,10 +114,11 @@ function watchTest(): any {
 }
 
 function bundleSpecs(done: Function): any {
+  'use strict';
 
-  var browserify = require('ionic-gulp-browserify-typescript');
-  let glob = require('glob');
-  let specs = glob.sync('**/*.spec.ts')
+  let browserify: any = require('ionic-gulp-browserify-typescript');
+  let glob: any = require('glob');
+  let specs: any = glob.sync('**/*.spec.ts');
 
   browserify(
     {
@@ -122,33 +129,54 @@ function bundleSpecs(done: Function): any {
       browserifyOptions: {
         cache: {},
         packageCache: {},
-        debug: true
+        debug: true,
       },
     }
   ).on('end', done);
 };
 
 function remapIstanbul(): any {
-  var remapIstanbul = require('remap-istanbul/lib/gulpRemapIstanbul');
+  'use strict';
+
+  let remapIstanbul: any = require('remap-istanbul/lib/gulpRemapIstanbul');
 
   return gulp.src(join(COVERAGE, 'istanbul-remap', 'coverage-final.json'))
     .pipe(remapIstanbul({
-        reports: {
-          'json': join(COVERAGE, 'istanbul-remap', 'coverage-remapped.json')
-        }
+      reports: {
+        'json': join(COVERAGE, 'istanbul-remap', 'coverage-remapped.json'),
+      },
     }));
 }
 
 function reportIstanbul(): any {
+  'use strict';
 
-  var istanbulReport = require('gulp-istanbul-report');
-  gulp.src(join(COVERAGE, 'istanbul-remap', 'coverage-remapped.json'))
+  let istanbulReport: any = require('gulp-istanbul-report');
+  gulp.src(join(COVERAGE, 'istanbul-remap', 'coverage-pruned.json'))
     .pipe(istanbulReport({
     reporters: [
       'text',
-      {name: 'lcovonly', file: 'lcov'}
-    ]
-  }))
+      {name: 'lcovonly', file: 'lcov'},
+    ],
+  }));
+}
+
+function pruneIstanbul(): any {
+  'use strict';
+
+  const toPrune: Array<string> = ['node_modules', '.spec.ts', '.d.ts'];
+  let fs: any = require('fs');
+  let remapped: any = JSON.parse(fs.readFileSync(join(COVERAGE, 'istanbul-remap', 'coverage-remapped.json')));
+  let pruned: Object = {};
+
+  Object.keys(remapped).forEach((key) => {
+    let doPrune: any = toPrune.find((glob) => (key.indexOf(glob) > -1));
+    // the find will return `undefined` if there is nothing to be pruned
+    if (doPrune) return;
+    pruned[key] = remapped[key];
+  });
+
+  fs.writeFileSync(join(COVERAGE, 'istanbul-remap', 'coverage-pruned.json'), JSON.stringify(pruned));
 }
 
 gulp.task('test.bundle.specs', bundleSpecs);
@@ -161,6 +189,7 @@ gulp.task('test.lint', lint);
 gulp.task('test.watch', watchTest);
 gulp.task('remap-istanbul', remapIstanbul);
 gulp.task('report-istanbul', reportIstanbul);
+gulp.task('prune-istanbul', pruneIstanbul);
 
 // just a hook into ionic's build
 gulp.task('ionic.build', (done: any) => {
@@ -185,7 +214,7 @@ gulp.task('test.bundle', (done: any) => {
     'html', // this is a hook into ionic
     'test.bundle.specs',
     done
-  )
+  );
 });
 
 // first time round we should nuke everything
@@ -205,12 +234,12 @@ gulp.task('test', (done: any) => {
   );
 });
 
-
 gulp.task('test.new', (done: any) => {
   runSequence(
     'test.bundle',
     'test.karma',
     'remap-istanbul',
+    'prune-istanbul',
     'report-istanbul',
     done
   );
