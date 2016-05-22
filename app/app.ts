f79a8bbda30ae02e52d558bc6b904454a4671e77
Stephen Hazleton <shazleto@gmail.com>
2016-05-22 18:02:54 +1200

    porting changes from ionic starter

--- app/app.ts
+++ app/app.ts
@@ -33,20 +33,9 @@ export class ClickerApp {
 
   private initializeApp(): void {
     this.platform.ready().then(() => {
-      // The platform is now ready. Note: if this callback fails to fire, follow
-      // the Troubleshooting guide for a number of possible solutions:
-      //
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      //
-      // First, let's hide the keyboard accessory bar (only works natively) since
-      // that's a better default:
-      //
-      // Keyboard.setAccessoryBarVisible(false);
-      //
-      // For example, we might change the StatusBar color. This one below is
-      // good for dark backgrounds and light text:
-      // StatusBar.setStyle(StatusBar.LIGHT_CONTENT)
+      StatusBar.styleDefault();
     });
   }
 
