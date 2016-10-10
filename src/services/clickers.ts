71540e87f356ea0a26b4e3a9034b2b2caa96c8d1
Stephen Hazleton <shazleto@gmail.com>
2016-10-10 22:23:47 +1100

    redefine let

--- src/services/clickers.ts
+++ src/services/clickers.ts
@@ -17,10 +17,16 @@ export class ClickersService {
     this.clickerActions = clickerActions;
     window['clickersService'] = this;
     window['ClickerSelector'] = ClickerSelector;
+
+    // apparently let has been removed from rxjs/Observalbe, it just does this:
+    // http://stackoverflow.com/questions/38340541/how-to-use-rx-observable-prototype-let-operator
+    store.let = function(fn: Function): any {
+      return fn(this);
+    };
   }
 
   public getData(): Observable<Clicker[]> {
-    return this.store.select(ClickerSelector.getClickerItems());
+    return this.store.let(ClickerSelector.getClickerItems());
   }
 
   public initialise(): void {
@@ -30,7 +36,7 @@ export class ClickersService {
   }
 
   public isFetching(): Observable<boolean> {
-    return this.store.select(
+    return this.store.let(
       ClickerSelector.getLoading()
     );
   }
