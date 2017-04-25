cbd5de37332ebd6e11b991640dedfae5b61f8628
Stephen Hazleton <shazleto@gmail.com>
2017-04-25 18:42:53 +1000

    removing typings that came in #243

--- typings/cordova-typings.d.ts
+++
@@ -1,4 +0,0 @@
-
-/// <reference path="../.vscode/typings/cordova/cordova.d.ts"/>
-/// <reference path="../.vscode/typings/cordova-ionic/plugins/keyboard.d.ts"/>
-/// <reference path="../.vscode/typings/jquery/jquery.d.ts"/>
