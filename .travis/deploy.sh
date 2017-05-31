753c3b7ac76f66b00bd399a062b7bfd4bebaf329
Stephen Hazleton <shazleto@gmail.com>
2017-06-01 07:50:55 +1000

    switching to LTS and adding deploy back

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,6 +4,6 @@
 #
 # deploy to davonez in addition to ionic view for debug
 # Ionic upload failing see https://github.com/ionic-team/ionic/issues/11872
-# ionic upload --email $IONIC_EMAIL --password $IONIC_PASSWORD
+ionic upload --email $IONIC_EMAIL --password $IONIC_PASSWORD
 # ssh-keyscan 176.58.107.25  >> ~/.ssh/known_hosts
 # scp -i .travis/travis_rsa.pem platforms/android/build/outputs/apk/android-debug.apk shazleto@176.58.107.25:~/clicker
