3e99ed0e00a8b66b8529d3e7606f268dbfbbf453
Stephen Hazleton <shazleto@gmail.com>
2017-07-31 11:09:43 +1000

    nosandbox

--- karma.conf.js
+++ karma.conf.js
@@ -38,7 +38,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    browsers: ['ChromeNoSandbox'],
+    singleRun: false,
+    customLaunchers: {
+      ChromeNoSandbox: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    }
   });
 };
