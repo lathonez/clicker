54573a485c971c7301f62283669047599053b985
Stephen Hazleton <shazleto@gmail.com>
2017-11-27 11:48:37 +1100

    import version directly from package.json

--- src/pages/clickerList/clickerList.ts
+++ src/pages/clickerList/clickerList.ts
@@ -1,6 +1,7 @@
 import { Component }       from '@angular/core';
 import { NavController }   from 'ionic-angular';
 import { ClickersService } from '../../services';
+import { version }         from '../../../package.json';
 
 @Component({
   templateUrl: './clickerList.html',
@@ -10,11 +11,14 @@ export class ClickerList {
 
   public clickerService: ClickersService;
   public title: string;
+  public version: string;
+
   private nav: NavController;
 
   constructor(nav: NavController, clickerService: ClickersService) {
     this.nav = nav;
     this.clickerService = clickerService;
     this.title = 'Clickers';
+    this.version = version;
   }
 }
